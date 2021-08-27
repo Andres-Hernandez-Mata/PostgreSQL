702e388954a43172efb69b476feac16113fc366b
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-08-27 14:13:54 -0500

    DATABASE postgres

---
+++ src/postgres.sql
@@ -0,0 +1,29 @@
+-- Database: postgres
+
+-- DROP DATABASE postgres;
+
+CREATE DATABASE postgres
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE postgres
+    IS 'default administrative connection database';
+	
+CREATE DATABASE foo
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+
+\c foo;
+
+
